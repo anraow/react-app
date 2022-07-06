4216bf707f3967dc9e5445e36ba0ff28f0ca6f3d
anraow <anraow@gmail.com>
2022-07-06 14:17:39 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="item">
+      <img src="cup.png" alt=""></img>
+      <div class="info">
+        Frappuccino <span>$4.99</span>
+      </div>
+      <button>ADD</button>
+    </div>
+  );
+}
+
+export default App;
